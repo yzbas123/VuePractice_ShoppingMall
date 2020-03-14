0b3cffbb29c2de1dc6a03282f1f9728eb911153b
yzbas123 <5314264343@qq.com>
2020-03-14 20:56:43 +0800

    use fastclick

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import toast from 'c_common/toast'
+import fastClick from 'fastclick'
+fastClick.attach(document.body);
 Vue.config.productionTip = false
 Vue.use(toast);
 Vue.prototype.$bus = new Vue();
